46b86549b8ad8da7fb5ce765834bf13ba3d5c236
gustav018 <gustavofleitas18@gmail.com>
2024-05-24 21:57:59 -0400

    actualizacion bd.php

--- server/bd.php
+++ server/bd.php
@@ -2,13 +2,8 @@
 $contraseña = "";
 $usuario = "root";
 $nombre_base_de_datos = "gym_store";
-
 try {
-    $conexion = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
-    // Establecer el modo de error de PDO a excepción
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa a la base de datos.";
-    return $conexion;
+    return new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
 } catch (Exception $e) {
     echo "Ocurrió algo con la base de datos: " . $e->getMessage();
 }
