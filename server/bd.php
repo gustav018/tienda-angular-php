e2b6cea6c836196672d77b95022999c248c0b852
gustav018 <gustavofleitas18@gmail.com>
2024-05-24 20:56:04 -0400

    Actualizacion de index del server

--- server/bd.php
+++ server/bd.php
@@ -2,8 +2,13 @@
 $contraseña = "";
 $usuario = "root";
 $nombre_base_de_datos = "gym_store";
+
 try {
-    return new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+    $conexion = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+    // Establecer el modo de error de PDO a excepción
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa a la base de datos.";
+    return $conexion;
 } catch (Exception $e) {
     echo "Ocurrió algo con la base de datos: " . $e->getMessage();
 }
